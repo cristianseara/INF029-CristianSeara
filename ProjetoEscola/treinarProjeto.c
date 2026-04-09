f5cbc0a409962c057bca74a95f33b48e679ffff7
Cristian Seara <20212160058@ifba.edu.br>
2026-04-09 20:19:43 +0000

    treinar sobre Listar Aluno

--- ProjetoEscola/treinarProjeto.c
+++ ProjetoEscola/treinarProjeto.c
@@ -1,103 +1,109 @@
 #include <stdio.h>
 #define TAM_ALUNO 3
 
-int main(){
-
+int main()
+{
     int sair = 0;
-    int listarAluno [TAM_ALUNO];
+    int listaAluno[TAM_ALUNO];
     int qtdAluno = 0;
-    int opcao;
+    int opcao; 
+    
     
 
-    while(!sair){
-
-    printf("\n.....Escola Projeto......\n");
+    while(!sair)
+    {
+    printf("\n....Projeto Escola....\n");
     printf(" 0 - Sair\n");
     printf(" 1 - Aluno\n");
     printf(" 2 - Professor\n");
-    printf(" 3 - Disciplina\n");
-
+    printf(" 3 - Dsiciplina\n");
+    
     scanf("%d", &opcao);
-
+    
     switch(opcao){
+        
         case 0:{
             sair = 1;
-            break;
+        break;
         }
         case 1:{
             printf("Modulo Aluno\n");
             int sairAluno = 0;
             int opcaoAluno;
-
+            
             while(!sair){
-              
-                printf(" 0 - Voltar Aluno\n");
-                printf(" 1 - Cadastrar Aluno\n");
-                printf(" 2 - Listar Aluno\n");
-                printf(" 3 - Atualizar Aluno\n");
-                printf(" 4 - Excluir Aluno\n");
-
-                scanf("%d",&opcaoAluno);
-
-                switch(opcaoAluno){
-                    case 0:{
-                        sairAluno = 1;
-                        break;
-                    }
-                    case 1:{
-                        printf(" Cadastrar Aluno\n");
-                        printf("Digite a matricula\n");
-                        int matricula;
-                        scanf("%d",&matricula);
-                        if(matricula < 0){
-                            printf("matricula invalido\n");
+            
+            printf(" 0 - Voltar\n");
+            printf(" 1 - Cadastrar Aluno\n");
+            printf(" 2 - Listar Aluno\n");
+            printf(" 3 - Atualizar Aluno\n");
+            printf(" 4 - Excluir Aluno\n");
+            
+            scanf("%d", &opcaoAluno);
+            
+            switch(opcaoAluno){
+                case 0:{
+                sairAluno = 0;
+                break;    
+                }
+                case 1:{
+                printf(" Cadastrar Aluno\n");
+                    int matricula;
+                    printf("Digite a matriucla\n");
+                    scanf("%d",&matricula);
+                    if(matricula < 0){
+                        printf("matricula invalido\n");
+                    }else{
+                        if(qtdAluno == TAM_ALUNO){
+                        printf("Listar de alunos cheia\n");
                         }else{
-                            if(qtdAluno == TAM_ALUNO){
-                                printf("matricula de alunos cheia\n");
-                            }else{
-                                listarAluno[qtdAluno] = matricula;
-                                qtdAluno++;
-                            }
+                            listaAluno [qtdAluno] = matricula;
+                            qtdAluno ++;
                         }
-                        break;
                     }
-                    case 2:{
-                        printf(" Listar Aluno\n");
-                        break;
-                    }
-                    case 3:{
-                        printf(" Atualizar Aluno\n");
-                        break;
-                    }
-                    case 4:{
-                        printf(" Excluir Aluno\n");
-                        break;
-                    }
-                    default:{
-                        printf ("opcao invalida\n");
-                        break;
-                    }
+                break;    
+                }
+                case 2:{
+                printf(" Listar Aluno\n");
+                for(int i = 0; i < TAM_ALUNO; i++);
+                
+                break;    
+                }
+                case 3:{
+                printf(" Atualizar Aluno\n");
+                break;    
+                }
+                case 4:{
+                printf(" Excluir Aluno\n");
+                break;    
+                }
+                default:{
+                printf(" opcao invalido\n");
+                break;    
                 }
             }
-
-            break;
+        }
+        break;
         }
         case 2:{
             printf("Modulo Professor\n");
-            break;
+        break;
         }
         case 3:{
-            printf("Modulo Disciplina\n");
-            break;
+            printf("Modulo Dsiciplina\n");
+        break;
         }
         default:{
-            printf("opcao invalida\n");
-            break;
+            printf("opcao invalido\n");
+        break;
         }
     }
 }
-
-
-
+    
+    
     return 0;
 }
+
+
+
+
